b38dac56bc1bfd41a9e8a35fd7a1cd2b2291f078
jaehoonx2 <21300698@handong.edu>
2019-01-06 15:53:59 +0900

    Saving the user name

--- clock.js
+++ clock.js
@@ -1,23 +1,19 @@
-// querySelector -> 자식 element 지정
 const clockContainer = document.querySelector(".js-clock"),
-      clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1");
 
-function getTime(){
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    //usage of `${var}`
-    clockTitle.innerText =
-        `${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds}`;
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
-function init(){
-    getTime();
-    setInterval(getTime, 1000);     // setInterval(func, millisecond);
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
